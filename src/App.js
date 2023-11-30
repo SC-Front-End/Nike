6898e7c8bee2f9fc2d0bb5a3e67ea5dcbeea48e6
SU KYUNG IM <99783474+oiosu@users.noreply.github.com>
2023-11-30 14:37:21 +0900

    feat: header 기본 스타일링 및 기본 구조

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import Header from "./components/Header";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          FE 01 팀 프로젝트 가이드 작성 중입니다.
-        </p>
-
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <Header />
+      </div>
+    </>
   );
 }
 
